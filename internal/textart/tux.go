8e426bffbb1be55ec98b50f4a84247f3e84b198a
Tom <tom@tdpain.net>
2021-01-05 17:53:52 +0000

    Add textart (because this is very important)

---
+++ internal/textart/tux.go
@@ -0,0 +1,20 @@
+package textart
+
+var Tux = []string{
+	"         _nnnn_",
+	"        dGGGGMMb",
+	"       @p~qp~~qMb",
+	"       M|@||@) M|",
+	"       @,----.JM|",
+	"      JS^\\__/  qKL",
+	"     dZP        qKRb",
+	"    dZP          qKKb",
+	"   fZP            SMMb",
+	"   HZM            MMMM",
+	"   FqM            MMMM",
+	" __| \".        |\\dS\"qML",
+	" |    `.       | `' \\Zq",
+	"_)      \\.___.,|     .'",
+	"\\____   )MMMMMP|   .'",
+	"     `-'       `--'",
+}
