a5b66b7a8e9a056b3adee5ecf0a87e19bc2d705c
Tom <tom@tdpain.net>
2021-01-07 12:33:26 +0000

    Update pet stats to array of structs

--- internal/pet/pet.go
+++ internal/pet/pet.go
@@ -3,11 +3,7 @@ package pet
 type Pet struct {
 	Name string
 
-	Health int
-	Boredom int
-	Thirst int
-	Hunger int
-	Fatigue int
+	Stats []PetStat
 }
 
 func NewPet(name string) *Pet {
@@ -16,10 +12,18 @@ func NewPet(name string) *Pet {
 
 	return &Pet{
 		Name:    name,
-		Health:  100,
-		Boredom: 0,
-		Thirst:  0,
-		Hunger:  0,
-		Fatigue: 0,
+		Stats: []PetStat{
+			{Name: "Health", Value: 100},
+			{Name: "Boredom", Value: 0},
+			{Name: "Thirst", Value: 0},
+			{Name: "Hunger", Value: 0},
+			{Name: "Fatigue", Value: 0},
+		},
 	}
+}
+
+type PetStat struct {
+	Name string
+	Value int
+	Delta int
 }
