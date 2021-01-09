a1e737026e206385a9ec3556c6e7b0f06ce62a69
Tom <tom@tdpain.net>
2021-01-09 18:54:53 +0000

    First sensible entrypoint

--- cmd/cyberpet/main.go
+++ cmd/cyberpet/main.go
@@ -2,26 +2,21 @@ package main
 
 import (
 	"github.com/codemicro/cs-cyberpet/internal/display"
-	"github.com/codemicro/cs-cyberpet/internal/textart"
-	"github.com/codemicro/cs-cyberpet/internal/tools"
-	"os"
-	"time"
+	"github.com/codemicro/cs-cyberpet/internal/game"
+	"github.com/codemicro/cs-cyberpet/internal/pet"
 )
 
 func main() {
+
+	defer func() {
+		display.BeforeShutdown()
+	}()
+
 	display.Scaffold()
-	screenX, screenY := display.Screen.Size()
-	tuxX, tuxY := display.FindTopLeftCoord(tools.FindLongestString(textart.Tux), len(textart.Tux), screenX, screenY-display.BottomLineHeight)
 
-	display.PrintMultiLine(textart.Tux, tuxX, tuxY)
+	pet.CurrentPet = pet.NewPet("Tux")
+	display.StartStatLoop(pet.CurrentPet)
 
-	display.Box(2, 1, 37, 8)
+	game.Play()
 
-	display.CharacterSay("heho am tux\nam here to steal pancakes", tuxX, tuxY, tools.FindLongestString(textart.Tux), 3)
-
-	inp, _ := display.CollectInputAtPosition(os.Stdin, 2, screenY-display.BottomLineHeight + 2, true, 0)
-
-	display.PrintString(inp, 0, 0)
-
-	time.Sleep(time.Second * 2)
 }
