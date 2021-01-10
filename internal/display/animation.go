3a4c3b040b31cbe10040452161aa016c2d4ab796
Tom <tom@tdpain.net>
2021-01-10 16:43:17 +0000

    Add animation for food to display
    display.AnimateSlideIn

---
+++ internal/display/animation.go
@@ -0,0 +1,38 @@
+package display
+
+import (
+	"github.com/codemicro/cs-cyberpet/internal/textart"
+	"github.com/codemicro/cs-cyberpet/internal/tools"
+	"time"
+)
+
+func AnimateSlideIn(image []string) {
+	screenX, _ := Screen.Size()
+
+	// find center point in screen Y
+	_, ypos := findTopLeftCoord(image, tools.FindLongestStringLen(image))
+
+	// find center point in screen X for the barrier
+	barrierX := screenX / 2
+
+	cf := func(){}
+
+	modimg := make([]string, len(image))
+	copy(modimg, image)
+
+	for xpos := 0; xpos < barrierX; xpos += 4 {
+
+		diff := barrierX - xpos
+		for i, ln := range modimg {
+			if len(ln) > diff {
+				modimg[i] = ln[:diff]
+			}
+		}
+
+		cf()
+		PrintTransparentMultiString(modimg, xpos, ypos)
+		cf = makeClearFunction(modimg, xpos, ypos)
+		ShowCharacterInCenter(textart.Tux)
+		time.Sleep(time.Second / 8)
+	}
+}
