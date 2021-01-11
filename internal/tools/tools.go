53388a912cbb7cf422ca4c003e685350c1be1353
Tom <tom@tdpain.net>
2021-01-11 17:30:40 +0000

    Add comments to package tools

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -1,5 +1,6 @@
 package tools
 
+// FindLongestStringLen returns the length of the longest string in a slice of strings
 func FindLongestStringLen(x []string) int {
 	var longest int
 	for _, y := range x {
@@ -10,6 +11,7 @@ func FindLongestStringLen(x []string) int {
 	return longest
 }
 
+// MakeRuneSlice returns a slice of runes of len length, with each run being rune r
 func MakeRuneSlice(r rune, length int) (o []rune) {
 	for i := 0; i < length; i += 1 {
 		o = append(o, r)
@@ -17,12 +19,16 @@ func MakeRuneSlice(r rune, length int) (o []rune) {
 	return
 }
 
+// RightPadString pads a string to a specific length with a specific character
 func RightPadString(in string, newLen int, pad rune) string {
 	for i := 0; i < newLen-len(in); i += 1 {
 		in += string(pad)
 	}
 	return in
 }
+
+// see https://github.com/CSCoursework/cs-battleships#numbering-the-board-with-letters for info about these two
+// functions
 
 func GetAlphabetChar(i int) string {
 	return string(rune(int('a') + i))
